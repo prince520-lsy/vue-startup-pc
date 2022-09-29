ae2648539b86c98fdcf8a40ba09e4b0afd99fc0c
JinJInLiang <3122702689@qq.com>
2022-09-29 14:20:33 +0800

    vue-82

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,16 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+      <div>Jack今年{{age1}}岁  {{age1>=18?'成年':'未成年'}}</div>
+      <div>Jane今年{{age2}}岁  {{age2>=18?'成年':'未成年'}}</div>
+      <div>{{num1}}+{{num2}}={{num1+num2}}</div>
+      <ul>
+        <li>Name:{{student.name}}</li>
+        <li>Age:{{student.age>=18?'成年':'未成年'}}</li>
+        <li>Sex:{{student.gender}}</li>
+      </ul>
+      <h1>今年是{{getYear()}}</h1>
+      <a :href="url">百度</a>
+      <img :src="urlLogo" alt="">
   </div>
 </template>
 
@@ -9,20 +18,35 @@
 import HelloWorld from './components/HelloWorld.vue'
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+  data() {
+    return {
+      age1: 10,
+      age2: 20,
+      num1: 100,
+      num2: 200,
+      student: {
+        name: 'jack',
+        age: '22',
+        gender:'boy'
+      },
+      url:'https://www.baidu.com/',
+      urlLogo: 'https://www.baidu.com/img/PCtm_d9c8750bed0b3c7d089fa7d55720d6cf.png',
+      ok: true,
+      notok: false,
+      user: 'Jack',
+      password:'123456'
+    }
+  },
+  methods: {
+    getYear() {
+
+            return new Date().getFullYear();
+        }
+    }
+  
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
